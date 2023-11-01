887ce9cfae8ab5ea153b2a7d746a7fdc3b3b273d
Carmen Regina <crcarmen23@gmail.com>
2023-11-01 13:52:11 -0500

    added code to index api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,9 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./postRoutes');
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+
+module.exports = router;
 
