69f2543aed0ba49053237004f77b6c5b112c57e1
Carmen Regina <crcarmen23@gmail.com>
2023-11-01 12:03:56 -0500

    added seed structure together

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment, Favorite } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+// const commentData = require('./commentData.json');
+// const favoriteData = require('./favoriteData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,12 +14,29 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const post of postData) {
+    await Post.create({
+      ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
+
+// const comment = await Comment.bulkCreate(commentData, {
+//   individualHooks: true,
+//   returning: true,
+// });
+
+// for (const comment of commentData) {
+//   await Comment.create({
+//     ...comment,
+//     user_id: users[Math.floor(Math.random() * users.length)].id,
+//   });
+// }
+
+// const favorite = await Favorite.bulkCreate(favoriteData, {
+//   individualHooks: true,
+//   returning: true,
+// });
 
   process.exit(0);
 };
