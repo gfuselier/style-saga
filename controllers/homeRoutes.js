d0479c921d8ff63dbab7ad89d6c273c70721c7f7
gfuselier <gisellefuselier@gmail.com>
2023-11-02 13:51:49 -0500

    commented out withAuth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -65,7 +65,9 @@ router.get('/', async (req, res) => {
     }
   });
 
-  router.get('/profile', withAuth, async (req, res) => {
+  router.get('/profile', 
+//   withAuth, 
+  async (req, res) => {
     try {
       // Find the logged in user based on the session ID
       const userData = await User.findByPk(req.session.user_id, {
