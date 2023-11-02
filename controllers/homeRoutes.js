c1d4b10c847494d0fadfd5f21c55cb4719e6c6e3
gfuselier <gisellefuselier@gmail.com>
2023-11-02 14:36:24 -0500

    started create post form

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -61,23 +61,24 @@ router.get('/post/:id',
   router.get('/profile', 
 //   withAuth, 
   async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Post}, { model: Comment}],
-      });
+    // try {
+    //   // Find the logged in user based on the session ID
+    //   const userData = await User.findByPk(req.session.user_id, {
+    //     attributes: { exclude: ['password'] },
+    //     include: [{ model: Post}, { model: Comment}],
+    //   });
   
-      const user = userData.get({ plain: true });
+    //   const user = userData.get({ plain: true });
   
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+      res.render('profile', 
+    //   {...user,
+    //     logged_in: true}
+        );
+    // } catch (err) {
+    //   res.status(500).json(err);
+    // }
+  }
+);
   
   router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
