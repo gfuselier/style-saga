3781245928f18c68c329954c47a5ee0a91a68309
gfuselier <gisellefuselier@gmail.com>
2023-11-10 11:44:04 -0600

    edited form in post.hbs and started form handler in post.js

--- public/js/post.js
+++ public/js/post.js
@@ -1,3 +1,21 @@
-//event listener for add a comment form 
-//function that executes fetch call to talk to comment routes 
-//
+const newCommentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const text = document.querySelector('#comment-text').value.trim();
+
+    if (text) {
+        const response = await fetch(`/api/posts`, {
+          method: 'POST',
+          body: JSON.stringify({ title, description, picture }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          alert('Make sure to write a comment before submitting.');
+        }
+      }
+}
