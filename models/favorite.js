1c42994ee2f5afd339bf8a75d1b2f294e7f1ca05
gfuselier <gisellefuselier@gmail.com>
2024-01-01 18:58:05 -0600

    changed seed data for users, comments, posts, and removed favorites

--- models/favorite.js
+++ models/favorite.js
@@ -1,44 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Favorite extends Model { }
+// class Favorite extends Model { }
 
-Favorite.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        favorited: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            },
-        },
-        post_id:
-        {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'post',
-                key: 'id',
-            },
-        }
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'favorite',
-    }
-);
+// Favorite.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true,
+//         },
+//         favorited: {
+//             type: DataTypes.BOOLEAN,
+//             allowNull: false,
+//             defaultValue: true,
+//         },
+//         user_id: {
+//             type: DataTypes.INTEGER,
+//             references: {
+//                 model: 'user',
+//                 key: 'id',
+//             },
+//         },
+//         post_id:
+//         {
+//             type: DataTypes.INTEGER,
+//             references: {
+//                 model: 'post',
+//                 key: 'id',
+//             },
+//         }
+//     },
+//     {
+//         sequelize,
+//         timestamps: false,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'favorite',
+//     }
+// );
 
-module.exports = Favorite;
+// module.exports = Favorite;
